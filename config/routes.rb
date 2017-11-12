4327d95b38734a8fbc6b02391a7376081b386492
Nico Cai <chefbig@gmail.com>
2017-11-12 01:00:19 +0000

    part 2 finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'customers/index'
+  root 'customers#index'
 
-  get 'customer/index'
+  get 'customers/alphabetized'
+
+  get 'customers/missing_email'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
